45d2c914bc19142851f27acd7d263bbee32bb511
Thanathorn Taewattanapanit <75652796+SkyEngineerME@users.noreply.github.com>
2023-08-10 15:29:54 +0700

    Update Reply.jsx

--- src/components/Reply.jsx
+++ src/components/Reply.jsx
@@ -1,3 +1,25 @@
-export const Reply = ({ userImagePath, username, replyText, likeNum }) => {
-  return <div>your code here...</div>;
+export const Reply = props => {
+  return <div>
+    <div className="d-flex gap-2 my-2 ps-5">
+          <img
+            src={props.userImagePath}
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#E5E7EB" }}
+          >
+            <span className="fw-semibold">{props.username}</span>
+            <br />
+            <span>{props.replyText}</span>
+            <div className="d-flex align-items-center gap-1">
+              <img src="/like.svg" width={20}></img>
+              <span className="text-muted">{props.likeNum} คน</span>
+            </div>
+          </div>
+        </div>
+  </div>;
 };
