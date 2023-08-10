b94b05abfb72b66d0ceb01deeffc076a078625ee
Thanathorn Taewattanapanit <75652796+SkyEngineerME@users.noreply.github.com>
2023-08-10 15:29:31 +0700

    Update Comment.jsx

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,14 +1,26 @@
-export const Comment = ({
-  userImagePath,
-  username,
-  commentText,
-  likeNum,
-  replies,
-}) => {
-  return (
-    <div>
-      your code here...
-      {/* map-loop render Reply component here */}
-    </div>
-  );
+export const Comments = props => {
+  return <div>
+    
+    <div className="d-flex gap-2 my-2">
+          <img
+            src={props.userImagePath}
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#E5E7EB" }}
+          >
+            <span className="fw-semibold">{props.username}</span>
+            <br />
+            <span>{props.commentText}</span>
+            <div className="d-flex align-items-center gap-1">
+              <img src="/like.svg" width={20}></img>
+              <span className="text-muted">{props.likeNum} คน</span>
+            </div>
+          </div>
+        </div>
+  </div>
 };
